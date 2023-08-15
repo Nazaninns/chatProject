4dd2981437d2f41e8b8c117f04b3ef637186d328
nazaninns <nazaninnsq@gmail.com>
2023-08-15 13:22:02 +0330

    edit delete

--- delete.php
+++ delete.php
@@ -1,16 +1,23 @@
 <?php
+include_once 'Connection.php';
 session_start();
+$connection=Connection::getInstance()->getPdo();
 if (isset($_POST['delete'])){
     $number = $_POST['delete'];
-    $userChat = json_decode(file_get_contents('./storage/chat.json'), true);
-    foreach ($userChat as $user => $data) {
-        if ($user + 1 == $number) {
-            $chat['message'] = '';
-            $chat['deletedByAdmin'] = $_SESSION['user']['userName'];
-            $chat['deletedTime'] = date("y/m/d h:i:s");
-            $chats[$user] = $chat;
+    //$userChat = json_decode(file_get_contents('./storage/chat.json'), true);
+    $stmt=$connection->prepare('select * from chat');
+    $stmt->execute();
+    $userChat=$stmt->fetchAll();
+    // foreach ($userChat as $user => $data) {
+    //     if ($user + 1 == $number) {
+    //         $chat['message'] = '';
+    //         $chat['deletedByAdmin'] = $_SESSION['user']['userName'];
+    //         $chat['deletedTime'] = date("y/m/d h:i:s");
+    //         $chats[$user] = $chat;
 
-        }}
-    file_put_contents('chat.json', json_encode($userChat, JSON_PRETTY_PRINT));
+    //     }}
+    //file_put_contents('chat.json', json_encode($userChat, JSON_PRETTY_PRINT));
+    $stmt=$connection->prepare('delete from chat where chat.id=:id');
+    $stmt->execute(['id'=>$number]);
     header('Location:homePage.php');
 }
